7401856c9bb0d6898ef9bcda85e807f27772cbd6
NYoung214 <74738889+NYoung214@users.noreply.github.com>
2021-01-14 15:34:58 -0600

    Improved menu and added exiting application function

--- lockedme/src/main/java/lockedme/MainMenu.java
+++ lockedme/src/main/java/lockedme/MainMenu.java
@@ -1,19 +1,46 @@
 package lockedme;
 
+import java.util.Scanner;
+
 public class MainMenu {
 	
+	static Scanner input = new Scanner(System.in);
+	
 	public static void showMainMenu() {
+		
+		String choice;
 		
 		System.out.println("--------------------------"
 				+ "\nApplication: LockedMe.com"
 				+ "\nDeveloper: Nathaniel Young"
 				+ "\n--------------------------");
 		
-		System.out.println("------- Main Menu --------"
-				+"\n1. Show Files"
-				+"\n2. File Operations"
-				+"\n1. Close Application"
-				+"\n--------------------------");
+		while(true) {
+			System.out.println("------- Main Menu --------"
+					+"\n1. Show Files"
+					+"\n2. File Operations"
+					+"\n3. Close Application"
+					+"\n--------------------------");
+			
+			choice = input.nextLine();
+			
+			switch(choice) {
+			case "1":
+				//FileOperator.showFiles();
+				break;
+			case "2":
+				//FileOperator.menu();
+				break;
+			case "3":
+				System.out.println("Closing the application...");
+				System.exit(0);
+				break;
+			}
+		}
+
+		
+		
+		
 	}
 	
 	
