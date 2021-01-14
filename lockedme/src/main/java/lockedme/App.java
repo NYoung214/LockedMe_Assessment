0fef3c10ae383cbb047ac373bce079432d6f23aa
NYoung214 <74738889+NYoung214@users.noreply.github.com>
2021-01-14 15:27:11 -0600

    Moved main menu to a separate class

--- lockedme/src/main/java/lockedme/App.java
+++ lockedme/src/main/java/lockedme/App.java
@@ -3,25 +3,9 @@ package lockedme;
 public class App {
 
 	public static void main(String[] args) {
-		showWelcomeScreen();
-		showMainMenu();
+		MainMenu.showMainMenu();
 
 	}
 	
-	private static void showWelcomeScreen() {
-		System.out.println("--------------------------");
-		System.out.println("Application: LockedMe.com");
-		System.out.println("Developer: Nathaniel Young");
-		System.out.println("--------------------------");
-		
-	}
-	
-	private static void showMainMenu() {
-		System.out.println("------- Main Menu --------");
-		System.out.println("1. Show Files");
-		System.out.println("2. File Operations");
-		System.out.println("1. Close Application");
-		System.out.println("--------------------------");
-	}
 
 }
---
+++ lockedme/src/main/java/lockedme/MainMenu.java
@@ -0,0 +1,20 @@
+package lockedme;
+
+public class MainMenu {
+	
+	public static void showMainMenu() {
+		
+		System.out.println("--------------------------"
+				+ "\nApplication: LockedMe.com"
+				+ "\nDeveloper: Nathaniel Young"
+				+ "\n--------------------------");
+		
+		System.out.println("------- Main Menu --------"
+				+"\n1. Show Files"
+				+"\n2. File Operations"
+				+"\n1. Close Application"
+				+"\n--------------------------");
+	}
+	
+	
+}
