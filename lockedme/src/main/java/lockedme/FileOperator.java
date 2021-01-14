e012224e3d73cca0b313fd520ba2cfb08aec424d
NYoung214 <74738889+NYoung214@users.noreply.github.com>
2021-01-14 16:15:50 -0600

    Added menu for add, delete, search, and back operation

--- lockedme/src/main/java/lockedme/FileOperator.java
+++ lockedme/src/main/java/lockedme/FileOperator.java
@@ -26,4 +26,38 @@ public class FileOperator {
 			System.out.println(file.getName());
 		}	
 	}
+	
+	public static void menu() {
+		
+		String choice;
+		
+		while(true) {
+			System.out.println("------- File Operations --------"
+					+"\n1. Add a file"
+					+"\n2. Delete a file"
+					+"\n3. Search for a file"
+					+"\n4. Back to Main Menu"
+					+"\n--------------------------");
+			
+			choice = MainMenu.input.nextLine();
+			
+			switch(choice) {
+			case "1":
+				//addFile();
+				break;
+			case "2":
+				//deleteFile();
+				break;
+			case "3":
+				//searchFile()
+				break;
+			case "4":
+				MainMenu.showMainMenu();
+				break;
+			default:
+				System.out.println("Invalid input. Please enter 1, 2, 3, or 4.");
+				break;
+			}
+		}
+	}
 }
