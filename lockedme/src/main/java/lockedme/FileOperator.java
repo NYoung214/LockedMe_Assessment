2946835204edc7f17298cddc140e223267d68e80
NYoung214 <74738889+NYoung214@users.noreply.github.com>
2021-01-14 17:22:41 -0600

    Added functionality to search for a file in the directory

--- lockedme/src/main/java/lockedme/FileOperator.java
+++ lockedme/src/main/java/lockedme/FileOperator.java
@@ -8,20 +8,28 @@ import java.util.List;
 public class FileOperator {
 	
 	private static List<File> fileList = new ArrayList<>();
+	private static List<String> nameList = new ArrayList<>();
 	
 	public static void showFiles() {
 		Collections.sort(fileList);
-		printFiles();
+		Collections.sort(nameList);
+		printName();
 	}
 	
 	public static void fillFiles() {
 		File files = new File(MainMenu.FOLDER);
-		for(File file : files.listFiles()) {
-			fileList.add(file);
+		if(files.list().length == 0) {
+			System.out.println("Directory is empty.");
+		}else {
+			for(File file : files.listFiles()) {
+				fileList.add(file);
+				nameList.add(file.getName());
+			}
 		}
+
 	}
 	
-	private static void printFiles() {
+	private static void printName() {
 		for(File file : fileList) {
 			System.out.println(file.getName());
 		}	
@@ -49,7 +57,7 @@ public class FileOperator {
 				//deleteFile();
 				break;
 			case "3":
-				//searchFile()
+				searchFile();
 				break;
 			case "4":
 				MainMenu.showMainMenu();
@@ -60,4 +68,25 @@ public class FileOperator {
 			}
 		}
 	}
+	
+	private static void searchFile() {
+		System.out.println("Enter file name.");
+		String query = MainMenu.input.nextLine();
+		search(query);
+	}
+	
+	private static void search(String q) {
+		if(fileList.isEmpty()) {
+			System.out.println("Directory is empty.");
+		}else {
+			if(nameList.contains(q)) {
+				System.out.println("The File "+q+" has been found.");
+			}else {
+				System.out.println("The File "+q+" was NOT found.");
+			}
+		}
+	}
+	
+	
+	
 }
