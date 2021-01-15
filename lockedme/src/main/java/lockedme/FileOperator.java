498d2f732b046d091f2d8a5aaf5c02c33a1f543c
NYoung214 <74738889+NYoung214@users.noreply.github.com>
2021-01-15 11:39:51 -0600

    Added functionality to add files to directory

--- lockedme/src/main/java/lockedme/FileOperator.java
+++ lockedme/src/main/java/lockedme/FileOperator.java
@@ -1,6 +1,9 @@
 package lockedme;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,12 +11,10 @@ import java.util.List;
 public class FileOperator {
 	
 	private static List<File> fileList = new ArrayList<>();
-	private static List<String> nameList = new ArrayList<>();
 	
 	public static void showFiles() {
 		Collections.sort(fileList);
-		Collections.sort(nameList);
-		printName();
+		printFiles();
 	}
 	
 	public static void fillFiles() {
@@ -23,13 +24,12 @@ public class FileOperator {
 		}else {
 			for(File file : files.listFiles()) {
 				fileList.add(file);
-				nameList.add(file.getName());
 			}
 		}
 
 	}
 	
-	private static void printName() {
+	private static void printFiles() {
 		for(File file : fileList) {
 			System.out.println(file.getName());
 		}	
@@ -51,7 +51,7 @@ public class FileOperator {
 			
 			switch(choice) {
 			case "1":
-				//addFile();
+				addFile();
 				break;
 			case "2":
 				//deleteFile();
@@ -72,19 +72,83 @@ public class FileOperator {
 	private static void searchFile() {
 		System.out.println("Enter file name.");
 		String query = MainMenu.input.nextLine();
-		search(query);
+		switch(search(query)) {
+		case 1:
+			System.out.println("The File "+query+" has been found.");
+			break;
+		case 0:
+			System.out.println("Directory is empty.");
+			break;
+		case -1:
+			System.out.println("The File "+query+" was NOT found.");
+			break;
+		}
 	}
 	
-	private static void search(String q) {
+	private static int search(String q) {
 		if(fileList.isEmpty()) {
-			System.out.println("Directory is empty.");
+			return 0;
 		}else {
-			if(nameList.contains(q)) {
-				System.out.println("The File "+q+" has been found.");
-			}else {
-				System.out.println("The File "+q+" was NOT found.");
+			if(fileList.contains(new File(MainMenu.FOLDER + q))) {
+				return 1;
 			}
+			return -1;
 		}
+		
+	}
+	
+	private static void addFile() {
+		//ask for directory to file
+		//Use [ C:\Users\NecroNyancer\Documents\blank.txt ] for testing
+		System.out.println("Insert file location:");
+		String filePath = MainMenu.input.nextLine();
+		try {
+			Path fromPath = Paths.get(filePath);
+			
+			//check if location is valid
+			if(fileCheck(fromPath) == -1) {
+				//if not return to menu
+				System.out.println(fromPath.getFileName()+" is not a valid file");
+				return;				
+			}
+			//if yes then search to see if file already exist in directory	
+			System.out.println(fromPath.getFileName()+" is valid");
+			System.out.println("Searching for "+fromPath.getFileName()+" in directory");
+			if(search(fromPath.getFileName().toString()) == 1){
+				//if file already exists in directory return
+				System.out.println("File already exists in directory");
+				return;
+			}else {
+				//file does not exist in directory, add it
+				try {
+					String toPath = MainMenu.FOLDER + fromPath.getFileName();
+					//copy file
+					Files.copy(fromPath, Paths.get(toPath));
+					//insert name into array list
+					fileList.add(new File(toPath));
+					System.out.println(fromPath.getFileName() + " has been successfully added");
+				}catch(Exception IOException) {
+					System.out.println(fromPath.getFileName()+" could not be added.");
+					return;
+				}
+			}
+
+					
+			
+			
+		}catch(Exception InvalidPathException) {
+			System.out.println(filePath+" is not a valid path");
+			return;
+		}
+	}
+	
+	private static int fileCheck(Path path) {
+		//check if location is valid and a file
+		if(!Files.exists(path) || Files.isDirectory(path)) {
+			//if not return to menu
+			return -1;
+		}
+		return 1;
 	}
 	
 	
